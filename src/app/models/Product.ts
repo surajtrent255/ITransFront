1569a02a093bdf9adf79a633d56c2d070b34ed31
idealSuraj <suraj.trent255@gmail.com>
2023-03-20 02:35:14 -0400

    product class created

---
+++ src/app/models/Product.ts
@@ -0,0 +1,14 @@
+export class Product {
+
+  name !: string;
+  sellingPrice !: number;
+  costPrice !: number;
+  userId !: number;
+  companyId !: number;
+  sellerId !: number;
+  categoryId !: number;
+  barcode !: string;
+  discount !: number;
+  tax !: number;
+
+}
