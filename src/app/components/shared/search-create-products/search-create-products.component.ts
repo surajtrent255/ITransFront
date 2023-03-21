cfa60a6364f457d41254c6f5f5b6b3f2330b93c2
idealSuraj <suraj.trent255@gmail.com>
2023-03-21 05:59:29 -0400

    catservicecreated

--- src/app/components/shared/search-create-products/search-create-products.component.ts
+++ src/app/components/shared/search-create-products/search-create-products.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { Product } from 'src/app/models/Product';
+import { CategoryProduct } from 'src/app/models/CategoryProduct';
+
 import { RJResponse } from 'src/app/models/rjresponse';
 import { ProductService } from 'src/app/service/product.service';
 import Swal from 'sweetalert2';
+import { CategoryProductService } from 'src/app/service/category-product.service';
 
 @Component({
   selector: 'app-search-create-products',
@@ -13,15 +16,19 @@ export class SearchCreateProductsComponent {
 
   product: Product = new Product();
   isFormSubmitable: boolean = false;
+  availableCategories: CategoryProduct[] = [];
 
   availableProducts: Product[] = []
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private categoryProductService: CategoryProductService) { }
 
   ngOnInit() {
     this.productService.getAllProducts().subscribe(data => {
-      console.log("data== ");
-      console.log(data);
       this.availableProducts = data.data
+    })
+    this.categoryProductService.getAllCategories().subscribe(data => {
+      console.log("categories " + data.data[0].id)
+      this.availableCategories = data.data;
+      console.log(this.availableCategories);
     })
   }
 
