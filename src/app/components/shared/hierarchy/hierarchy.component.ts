0f70020da29ed40a30a52492d62ad1b9c34a4c99
nabinjoshi400@gmail.com <nabinjoshi400@gmail.com>
2023-03-31 16:52:11 +0545

    menu fixed

--- src/app/components/shared/hierarchy/hierarchy.component.ts
+++ src/app/components/shared/hierarchy/hierarchy.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-hierarchy',
@@ -9,4 +10,25 @@ import { Component, ViewEncapsulation } from '@angular/core';
   ],
   encapsulation: ViewEncapsulation.None,
 })
-export class HierarchyComponent {}
+export class HierarchyComponent implements OnInit {
+  ngOnInit(): void {
+    // Initialize Bootstrap collapse
+    document.querySelectorAll('.sidebar .nav-link').forEach(function (element) {
+      element.addEventListener('click', function (e) {
+        let nextEl = element.nextElementSibling;
+        let parentEl = element.parentElement;
+
+        if (nextEl) {
+          e.preventDefault();
+          let mycollapse = new bootstrap.Collapse(nextEl);
+
+          if (nextEl.classList.contains('show')) {
+            mycollapse.hide();
+          } else {
+            mycollapse.show();
+          }
+        }
+      });
+    });
+  }
+}
