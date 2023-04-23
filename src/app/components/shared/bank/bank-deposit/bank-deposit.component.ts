b05b9f7794b70cb71894ad452d11da832c8af519
nabinjoshi400@gmail.com <nabinjoshi400@gmail.com>
2023-04-24 00:39:17 +0545

    bank module

---
+++ src/app/components/shared/bank/bank-deposit/bank-deposit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-deposit',
+  templateUrl: './bank-deposit.component.html',
+  styleUrls: ['./bank-deposit.component.css']
+})
+export class BankDepositComponent {
+
+}
---
+++ src/app/components/shared/bank/create-bank/create-bank.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-bank',
+  templateUrl: './create-bank.component.html',
+  styleUrls: ['./create-bank.component.css']
+})
+export class CreateBankComponent {
+
+}
